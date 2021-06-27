18946570dcc44f648dacc9b14e31a7ebf2ce7153
Scum3666 <Scum3666@gmail.com>
2021-06-27 14:09:01 +0800

    Запустил.

--- app/src/main/java/com/example/alculator/MainActivity.java
+++ app/src/main/java/com/example/alculator/MainActivity.java
@@ -12,20 +12,92 @@ import androidx.appcompat.widget.AppCompatImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
     private EditText result;
     private AppCompatImageButton backspace;
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button13);
         button1 = findViewById(R.id.button8);
+        button2 = findViewById(R.id.button12);
+        button3 = findViewById(R.id.button17);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button11);
+        button6 = findViewById(R.id.button16);
+        button7 = findViewById(R.id.button3);
+        button8 = findViewById(R.id.button10);
+        button9 = findViewById(R.id.button15);
         result = findViewById(R.id.result);
         backspace = findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("0");
+            }
+        });
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 result.append("1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("3");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("6");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("7");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.append("9");
             }
         });
         backspace.setOnClickListener(new View.OnClickListener() {
